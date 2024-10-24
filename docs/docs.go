3ce63b9f194265918c6c037f01d2129ea266a227
nurdiansyah15 <nurdinurdiansyah15@gmail.com>
2024-10-24 19:34:47 +0700

    wkwk

--- docs/docs.go
+++ docs/docs.go
@@ -7809,7 +7809,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "https://api.hertod-anal.cloud",
 	BasePath:         "/api/",
 	Schemes:          []string{},
 	Title:            "ML Master Data API",
